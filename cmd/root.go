6404fbd10b37283e0298ae9e755534008007ed41
72mins <72min@proton.me>
2024-03-01 23:19:51 +0100

    feat: Initialize CLI tool and add simple commands

---
+++ cmd/root.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gosniff",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	err := captureCmd.MarkPersistentFlagRequired("interface")
+	if err != nil {
+		return
+	}
+}
---
+++ pkg/listDevices/listDevices.go
@@ -0,0 +1,35 @@
+package listDevices
+
+import (
+	"fmt"
+	"github.com/google/gopacket/pcap"
+	"github.com/jwalton/gchalk"
+	"log"
+)
+
+func ListDevices() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(gchalk.Red("Error finding devices: " + err.Error()))
+	}
+
+	deviceCount := len(devices)
+	fmt.Println(gchalk.WithBgGreen().Black("Found " + fmt.Sprintf("%d", deviceCount) + " devices:"))
+
+	for _, device := range devices {
+		fmt.Println(gchalk.Green("Device name: " + device.Name))
+		fmt.Println(gchalk.Green("Description: " + device.Description))
+
+		addressCount := len(device.Addresses)
+		fmt.Println(gchalk.Green("Addresses (" + fmt.Sprintf("%d", addressCount) + "):"))
+
+		for _, address := range device.Addresses {
+			fmt.Println(gchalk.Green("   IP: " + address.IP.String()))
+			fmt.Println(gchalk.Green("   Netmask: " + address.Netmask.String()))
+			fmt.Println(gchalk.Green("   Broadcast: " + address.Broadaddr.String()))
+			fmt.Println(gchalk.Yellow("----------------------------------------------------------"))
+		}
+
+		fmt.Println(gchalk.Magenta("----------------------------------------------------------"))
+	}
+}
