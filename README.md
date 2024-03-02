6cc8f1c7d5fed281bbcfe354cd4dcaca52c5ac9a
72mins <72min@proton.me>
2024-03-02 11:36:07 +0100

    feat: Created live packet capture on single interface

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Needs
 
 For windows - npcap
+
+For ubuntu: sudo apt-get install libpcap-dev
--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -12,20 +9,9 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gosniff",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A CLI Network Analyzer Tool / Packet Sniffer written in Go",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
