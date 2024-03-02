649c1369c519d5a64a6adc2820881acc1cc5c418
tumpa02 <jtumpa@gmail.com>
2024-03-02 14:31:17 +0100

    feat: Added count flag to capture n number of packets.
    Added better showcasing of packet data.

--- cmd/capture.go
+++ cmd/capture.go
@@ -5,15 +5,16 @@ import (
 	"gosniff/pkg/capturePackets"
 )
 
-// captureCmd represents the capture command
 var captureCmd = &cobra.Command{
 	Use:   "capture",
 	Short: "Capture packets on a network interface",
 
 	Run: func(cmd *cobra.Command, args []string) {
 		iface, _ := cmd.Flags().GetString("interface")
+		promisc, _ := cmd.Flags().GetString("promiscuous")
+		count, _ := cmd.Flags().GetInt("count")
 
-		err := capturePackets.LiveCapture(iface)
+		err := capturePackets.LiveCapture(iface, promisc, count)
 		if err != nil {
 			return
 		}
@@ -23,5 +24,7 @@ var captureCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(captureCmd)
 
-	captureCmd.PersistentFlags().StringP("interface", "i", "", "The network interface to sniff for packets")
+	captureCmd.PersistentFlags().StringP("interface", "i", "any", "The network interface to sniff for packets")
+	captureCmd.PersistentFlags().StringP("promiscuous", "p", "false", "Whether to capture packets in promiscuous mode")
+	captureCmd.PersistentFlags().IntP("count", "c", 0, "The number of packets to capture before stopping (by default, it captures indefinitely)")
 }
